50530990e2682374fd55bf3059055ae4d899205b
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2026-07-25 23:48:23 +0100

    Add basename and 404 fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
@@ -8,20 +8,24 @@ import NotFound from "./pages/NotFound/NotFound";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 
-
-const router = createHashRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+			children: [
+				{ index: true, element: <Home /> },
+				{ path: "projects", element: <Projects /> },
+				{ path: "about", element: <About /> },
+				{ path: "contact", element: <Contact /> },
+				{ path: "*", element: <NotFound /> },
+			],
+		},
+	],
 	{
-		path: "/",
-		element: <App />, 
-		children: [
-			{ index: true, element: <Home /> },
-			{ path: "/projects", element: <Projects /> },
-			{ path: "/about", element: <About /> },
-			{ path: "/contact", element: <Contact /> },
-			{ path: "*", element: <NotFound /> },
-		],
+		basename: "/Portfolio",
 	},
-]);
+);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
